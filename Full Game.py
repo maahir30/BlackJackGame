899d5d5d9997243112fc9227e8211c7a8f5e6cf9
maahir30 <51933563+maahir30@users.noreply.github.com>
2020-05-19 13:47:19 -0700

    added more spaces

--- Full Game.py
+++ Full Game.py
@@ -144,7 +144,7 @@ def push(player,dealer,chips):
     print('Player and Dealer have tied. PUSH') 
 
 game_on = True
-print('Welcome to BlackJack!')
+print('\n\n\n\nWelcome to BlackJack!')
 while game_on:
 
     
