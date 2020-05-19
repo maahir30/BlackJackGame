8da158278767a40aa8b7ba695fd8515c8aedfb31
maahir30 <51933563+maahir30@users.noreply.github.com>
2020-05-18 18:12:37 -0700

    fixed win bet

--- Full Game.py
+++ Full Game.py
@@ -65,7 +65,7 @@ class Chips:
         self.bet = 0
         
     def win_bet(self):
-        self.total = self.total - self.bet
+        self.total = self.total + self.bet
     
     def lose_bet(self):
         self.total = self.total - self.bet 
